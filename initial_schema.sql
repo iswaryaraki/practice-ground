8872fd43d33d879524a7aa21049063a5fcf8ef44
Iswarya Raki <Iswarya Raki>
2017-03-08 20:26:54 -0500

    initial schema query

---
+++ initial_schema.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE play_ground
+	(`Id` int, `Name` varchar(7), `Description` varchar(55))
+;
+	
+INSERT INTO play_ground
+	(`Id`, `Name`, `Description`)
+VALUES
+	(1, 'SQL', 'Structured Query Language'),
+	(2, 'PLSQL', 'Procedural language'),
+	(3, 'Oracle', 'Oracle procedural language')
+;
